1a5decce49e805f39cddfa0881d98b29cd36c50f
d361120041 <d361120041@yahoo.com.tw>
2025-04-23 00:05:46 +0800

    新增social主頁面

---
+++ src/daniel/views/Social.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Social</h1>
+    <h1>貼文列表</h1>
+    <PostList />
+</template>
+
+<script setup>
+import PostList from '@/daniel/components/PostList.vue'
+</script>
+
+<style scoped>
+body {
+    font-family: Arial, sans-serif;
+    padding: 20px;
+}
+</style>
