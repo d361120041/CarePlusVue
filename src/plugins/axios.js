c55da26143fafd22d18b0575ee5a446b577a680c
daniel <d361120041@yahoo.com.tw>
2025-04-28 15:12:01 +0800

    修改axios.js的排序，增加預設攔截403狀態的註解

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
-const myAxios = axios.create({ 
+const myAxios = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
+});
+
+// 攔截從後端傳回的所有403狀態
+myAxios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    console.log("interceptors error", error);
+    if (error.response && error.response.status && error.response.status === 403) {
+        window.location.href = "/403";
+    }
+    return Promise.reject(error);
 });
 
 //yuuhou的新增
 myAxios.interceptors.request.use(config => {
     const token = localStorage.getItem('token');        // 你自己的 JWT 儲存位置
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-  }, error => Promise.reject(error));
+}, error => Promise.reject(error));
 //yuuhou的修改
-
-
-myAxios.interceptors.response.use(function(response) {
-    return response;
-}, function(error) {
-    console.log("interceptors error", error);
-    if(error.response && error.response.status && error.response.status === 403) {
-        window.location.href = "/403";
-    }
-    return Promise.reject(error);
-});
 
 export default myAxios;
