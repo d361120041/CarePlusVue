deee8e6ec76967b1106b29755a434b516df2afca
daniel <d361120041@yahoo.com.tw>
2025-04-28 12:19:21 +0800

    新增fontawesome相關套件及配置設定

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add( far)
+
 import router from '@/router/router.js'
 import App from '@/App.vue'
 
@@ -11,6 +16,7 @@ const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate)
 
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .use(pinia)
     .mount('#app')
