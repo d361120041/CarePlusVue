0f33f339dcf6eca31c9ceef559998715b4644d3d
daniel <d361120041@yahoo.com.tw>
2025-04-28 15:04:20 +0800

    修改router.js，增加註解區分各自的區塊

--- src/router/router.js
+++ src/router/router.js
@@ -1,39 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+// ================== 匯入套件 開始==================
+//  基礎頁面 
 import Home from '@/views/Home.vue'
 import NotFound from '@/views/NotFound.vue'
 import Forbidden from '@/views/Forbidden.vue'
-import Social from '@/daniel/views/Social.vue'
-import CaregiverLogin from '@/yuuhou/CaregiverLogin.vue'
+
+// Course
 import CourseHome from '@/yuni/views/CourseHome.vue'
 import CourseAdmin from '@/yuni/views/admin/CourseAdmin.vue'
-const routes = [
-    { path: "/", component: Home, name: "home" },
-    { path: "/:pathMatch(.*)", component: NotFound, name: "notfound" },
-    { path: "/403", component: Forbidden, name: "forbidden" },
-    { path: "/social", component: Social, name: "social" },
-    { path: "/caregiverLogin", component: CaregiverLogin, name: "caregiverLogin" },
-    { path: "/course", component: CourseHome, name: "courseHome" },
-    { path: "/course/admin", component: CourseAdmin, name: "courseAdmin" },
-];
 
+// Caregiver
+import CaregiverLogin from '@/yuuhou/CaregiverLogin.vue'
+
+// Social
+import Social from '@/daniel/views/Social.vue'
+// ================== 匯入套件 結束==================
+
+
+// ================== 路由設定 開始 ==================
+const routes = [
+  // 基礎頁面
+  { path: "/", component: Home, name: "home" },
+  { path: "/:pathMatch(.*)", component: NotFound, name: "notfound" },
+  { path: "/403", component: Forbidden, name: "forbidden" },
+
+  // Course
+  { path: "/course", component: CourseHome, name: "courseHome" },
+  { path: "/course/admin", component: CourseAdmin, name: "courseAdmin" },
+
+  // Caregiver
+  { path: "/caregiverLogin", component: CaregiverLogin, name: "caregiverLogin" },
+  
+  // Social
+  { path: "/social", component: Social, name: "social" },
+];
+// ================== 路由設定 結束 ==================
+
+
+// ================== 其他設定 開始 ==================
 const router = createRouter({
-    routes: routes,
+  routes: routes,
     history: createWebHistory(),
     linkActiveClass: 'active',
     linkExactActiveClass: 'active--exact'
-})
-
-router.beforeEach((to, from, next) => {
-    const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true'
-    if (to.path === '/caregiver' && !isAuthenticated) {
-      alert('請先登入')
-      next('/caregiverLogin')
-    } else {
-      next()
-    }
   })
 
+  router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true'
+  if (to.path === '/caregiver' && !isAuthenticated) {
+    alert('請先登入')
+    next('/caregiverLogin')
+  } else {
+    next()
+  }
+})
+// ================== 其他設定 結束 ==================
 
 export default router
 
