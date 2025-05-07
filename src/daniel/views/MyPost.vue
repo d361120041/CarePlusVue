74076b007963c5d73b0058cef452eda0d9392456
daniel <d361120041@yahoo.com.tw>
2025-05-07 21:08:11 +0800

    MyPost初始化，匯入PostMain元件

--- src/daniel/views/MyPost.vue
+++ src/daniel/views/MyPost.vue
@@ -1,11 +1,34 @@
 <template>
-    我的貼文
+    <div class="mypost-container">
+
+        <!-- 左欄：使用者資訊 -->
+        <aside class="user-info">
+            <img :src="authStore.user.profilePicture || '/default-avatar.png'" alt="頭貼" class="user-avatar" />
+            <h2 class="user-name">
+                {{ authStore.user.userName }}
+            </h2>
+            <p class="user-bio">{{ authStore.user.bio }}</p>
+            <p class="user-intro">{{ authStore.user.intro }}</p>
+        </aside>
+
+        <!-- 右欄：我的貼文列表 -->
+        <main class="mypost-main">
+            <h3 class="mypost-header">我的貼文</h3>
+                <PostMain />
+        </main>
+    </div>
 </template>
-    
+
 <script setup>
-    
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore()
+
+import PostMain from '@/daniel/components/post/PostMain.vue'
 </script>
-    
-<style>
-    
+
+<style scoped>
+.mypost-main {
+
+}
 </style>
