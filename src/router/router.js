70b9cb2a99deeafae21709a2df0ff6c54795717b
YuuHou1116 <ky942220707@gmail.com>
2025-05-13 14:06:09 +0800

    空格push

--- src/router/router.js
+++ src/router/router.js
@@ -122,7 +122,7 @@ const routes = [
   // ------------------ allen ------------------
 
   // ------------------ yuuhou ------------------
-
+ 
   {
     path: "/caregiver",
     component: CaregiverDashboard,
