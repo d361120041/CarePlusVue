4aee04e17cf166500cb1ebc47094447803f78326
YuuHou1116 <ky942220707@gmail.com>
2025-05-13 17:44:41 +0800

    commit 5/13

--- src/api/auth.js
+++ src/api/auth.js
@@ -15,8 +15,11 @@ export default {
   login(email, password) {
     return myAxios.post('/api/auth/login', { email, password })
   },
-  register(payload) {
-    return myAxios.post('/api/auth/register', payload)
+  register(data) {
+    return myAxios.post('/api/auth/register', data)
+  },
+  verify(data) {
+    return myAxios.post('/api/auth/verify', data)
   },
   forgotPassword(email) {
     return myAxios.post('/api/auth/forgot-password', { email })
