0907698c425afb734a20186cc237b7a273234815
daniel <d361120041@yahoo.com.tw>
2025-05-18 22:13:48 +0800

    增加avatar懶載入

--- src/daniel/components/user/UserAvatar.vue
+++ src/daniel/components/user/UserAvatar.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <router-link to="/user-center/myPost"> -->
-    <img class="user-avatar" :src="src" alt="User Avatar" />
+    <img class="user-avatar" :src="src" alt="User Avatar" loading="lazy" />
     <!-- </router-link> -->
 
 </template>
